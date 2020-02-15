37e1ef056b6407298c16b9c207fc044109b0d0db
Alexander Evteev <9284108@mail.ru>
2020-02-15 07:37:59 +0300

    Парсинг через стрим, станции пересадки

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,62 +3,55 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-import java.util.ArrayList;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
         try {
             final String LINK = "https://ru.wikipedia.org/wiki/Список_станций_Московского_метрополитена";
-//            final String LINK = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
-
             Document doc = Jsoup.connect(LINK).userAgent("Mozilla").maxBodySize(0).get();
-            Elements tables = doc.select("table");
 
-            for (Element table : tables) {
-                String tableHTML = table.outerHtml();
+//            final String FILE = "src/main/resources/MosMetro.html";
+//            Document doc = Jsoup.parse(readFile(FILE));
 
-                if (tableHTML.contains("standard sortable")) { // TODO: Переделать через html-тэги
-                    Document tableDoc = Jsoup.parse(tableHTML); // TODO: Убрать второй парсинг
+            Element table = doc.selectFirst("table.standard.sortable");
 
-                    Elements rows = tableDoc.select("tr");
-
-                    for (int j = 1; j < rows.size(); j++) {
-                        Element row = rows.get(j);
-                        Elements columns = row.select("td");
-                        if (columns.size() == 0) continue;
-
-                        String[] lineNums = columns.get(0).text().
-                                substring(0, columns.get(0).text().length() - 2). // TODO: Переделать через html-тэги
-                                split(" ");
-
-                        for (String lineNumber : lineNums) {
-
-                            lineNumber = lineNumber.replaceAll("^0", "");
-
-                            String lineName = columns.get(0).select("span").get(1).attr("title");
-
-                            String stationName = columns.get(1).select("a").first().text();
-
-                            ArrayList<String> connections = new ArrayList<>();
-                            for (Element connection : columns.get(3).select("span")) {
-                                if (!connection.text().isEmpty()) {
-                                    connections.add(connection.text().
-                                            replaceAll("^0", ""));
-                                }
-                            }
-                            System.out.printf("%-7s\t%-32s\t%-24s\t%s\n",
-                                    lineNumber,
-                                    lineName,
-                                    stationName,
-                                    connections);
-                        }
-                    }
-                }
+            Elements trs = table.select("tr");
+            for (int i = 1; i < trs.size(); i++) {
+                Element tr = trs.get(i);
+                Elements tds = tr.select("td");
+                System.out.printf("%-7s\t%-32s\t%-24s\t%8s\t%8s\n",
+                        tds.get(0).selectFirst("span").text().replaceAll("^0", ""),
+                        tr.select("span").get(1).attr("title"),
+                        tds.get(1).selectFirst("a").text(),
+                        tds.get(3).select("span.sortkey").stream()
+                                .map(e -> e.text()
+                                        .replaceAll("^0", ""))
+                                .collect(Collectors.joining("|")),
+                        tds.get(3).select("span[title]").stream()
+                                .map(e -> e.attr("title")
+                                        .replaceAll("Переход на станцию ", "")
+                                        .replaceAll("Московского центрального кольца", "")
+                                        .replaceAll("Кросс-платформенная пересадка на станцию ", "")
+                                        .replaceAll("[А-Яа-яё]+-*[А-Яа-яё]+\\s[А-Яа-яё]+$", "")
+                                        .trim())
+                                .collect(Collectors.joining("|")));
             }
-        } catch (IOException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+//    private static String readFile(String file) throws IOException {
+//        StringBuilder sb = new StringBuilder();
+//        List<String> lines = Files.readAllLines(Paths.get(file));
+//        lines.forEach(line -> sb.append(line).append("\n"));
+//        return sb.toString();
+//    }
 }
